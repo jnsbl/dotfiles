be3626a751c19fa0c992836cca0b37c15f9d7da4
Jan Sibil <jnsbldev@gmail.com>
2022-05-12 21:22:44 +0200

    Fix TERM value

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -x -g TERM "xterm-256color"
+set -x -g TERM "xterm-kitty"
 set -x -g EDITOR "nvim"
 set -x -g VISUAL "nvim"
 set -g fish_user_paths "$HOME/dotfiles/bin" "/usr/local/sbin" $fish_user_paths
