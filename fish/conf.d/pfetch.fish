7f3eea1438abb087349588f05d26b84278997097
Jan Sibil <jnsbldev@gmail.com>
2022-07-06 21:44:14 +0200

    Use pfetch as fish greeting

---
+++ fish/conf.d/pfetch.fish
@@ -0,0 +1,2 @@
+set -x -g SHELL "fish"
+set -x -g PF_INFO "ascii title os host kernel wm shell uptime"
--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,8 +1,12 @@
 function fish_greeting
-  echo '
-  _____ _     _
- |   __|_|___| |_
- |   __| |_ -|   |
- |__|  |_|___|_|_|
-                  ' | lolcat
+  if type -q pfetch
+    pfetch
+  else
+    echo '
+    _____ _     _
+   |   __|_|___| |_
+   |   __| |_ -|   |
+   |__|  |_|___|_|_|
+                    ' | lolcat
+  end
 end
