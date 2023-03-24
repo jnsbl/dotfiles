43c502c9da1bef8d2306107665ca77f380830195
Jan Sibil <jnsbldev@gmail.com>
2023-03-24 16:47:54 +0100

    Update cmdline and search completion
    nvim-cmp with cmdline had been giving me duplicate suggestions,
    that's why I replaced it with wilder-nvim (which I thought I was aready using)

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -31,28 +31,10 @@ cmp.setup.filetype("gitcommit", {
   })
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline("/", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = "buffer" }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" }
-  }, {
-    { name = "cmdline" }
-  })
-})
-
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require("lspconfig")
-lspconfig["sumneko_lua"].setup {
+lspconfig["lua_ls"].setup {
   capabilities = capabilities
 }
 lspconfig["bashls"].setup {
--- neovim/lua/plugins/wilder-nvim.lua
+++ neovim/lua/plugins/wilder-nvim.lua
@@ -19,41 +19,34 @@ local highlighters = {
   wilder.basic_highlighter(),
 }
 
--- wilder.set_option('renderer', wilder.renderer_mux({
---   [':'] = wilder.popupmenu_renderer({
---     highlighter = highlighters,
---   }),
---   ['/'] = wilder.wildmenu_renderer({
---     highlighter = highlighters,
---   }),
--- }))
-
--- -- Customising the renderer / Wildmenu renderer / Minimal theme
--- wilder.set_option('renderer', wilder.wildmenu_renderer({
---   highlighter = wilder.basic_highlighter(),
---   separator = ' · ',
---   left = {' ', wilder.wildmenu_spinner(), ' '},
---   right = {' ', wilder.wildmenu_index()},
--- }))
-
--- -- Customising the renderer / Popupmenu renderer / #2
--- wilder.set_option('renderer', wilder.renderer_mux({
---   [':'] = wilder.popupmenu_renderer({
---     highlighter = wilder.basic_highlighter(),
---   }),
---   ['/'] = wilder.wildmenu_renderer({
---     highlighter = wilder.basic_highlighter(),
---   }),
--- }))
-
--- Customising the renderer / Popupmenu renderer / Popupmenu borders
-wilder.set_option('renderer', wilder.popupmenu_renderer(
-  wilder.popupmenu_border_theme({
-    highlights = {
-      border = 'Normal', -- highlight to use for the border
-    },
-    -- 'single', 'double', 'rounded' or 'solid'
-    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-    border = 'rounded',
-  })
-))
+wilder.set_option('renderer', wilder.renderer_mux({
+  [':'] = wilder.popupmenu_renderer(
+    wilder.popupmenu_border_theme({
+      highlights = {
+        border = 'Normal',
+      },
+      border = 'rounded',
+      highlighter = highlighters,
+      left = {' ', wilder.popupmenu_devicons()},
+      right = {' ', wilder.popupmenu_scrollbar()},
+    })
+  ),
+  ['/'] = wilder.popupmenu_renderer(
+    wilder.popupmenu_border_theme({
+      highlights = {
+        border = 'Normal',
+      },
+      border = 'rounded',
+      highlighter = highlighters,
+    })
+  ),
+  ['?'] = wilder.popupmenu_renderer(
+    wilder.popupmenu_border_theme({
+      highlights = {
+        border = 'Normal',
+      },
+      border = 'rounded',
+      highlighter = highlighters,
+    })
+  ),
+}))
