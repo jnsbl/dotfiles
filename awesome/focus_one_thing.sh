50790c7c349298d0ed9188e166a3b458390ec55c
Jan Sibil <jnsbldev@gmail.com>
2023-03-26 15:58:58 +0200

    Tweak focus_one_thing.sh
    Sequence '.:.' looks a bit like a crosshair and is unobtrusive

--- awesome/focus_one_thing.sh
+++ awesome/focus_one_thing.sh
@@ -2,4 +2,4 @@
 set -e
 focus_file=~/.focus_one_thing
 one_thing=$(cat <(printf '(no focus)|') $focus_file | rofi -sep '|' -dmenu -p 'Focus' -theme ~/.config/rofi/launcher_colorful_style5.rasi)
-echo -e "$one_thing" | cat > $focus_file
+echo -e "$one_thing" | sed 's/(no focus)/.:./' | cat > $focus_file
