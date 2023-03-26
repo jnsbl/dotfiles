f743e1116ab0c1f243b431e5d3ef483402601a1b
Jan Sibil <jnsbldev@gmail.com>
2023-03-26 15:51:31 +0200

    Customize telescope-nvim mappings

--- neovim/lua/plugins/telescope-nvim.lua
+++ neovim/lua/plugins/telescope-nvim.lua
@@ -1,2 +1,15 @@
 local telescope = require("telescope")
 telescope.load_extension("zoxide")
+
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous
+      }
+    }
+  }
+}
