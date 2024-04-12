5e73d8ac359b42a40011fac3afa7e92bb16b0279
Jan Sibil <jnsbldev@gmail.com>
2024-04-12 20:47:04 +0200

    Update bootstrap-hyprland.sh

--- bootstrap-hyprland.sh
+++ bootstrap-hyprland.sh
@@ -5,22 +5,25 @@ set -eu
 # DRY_RUN="--dry-run"
 DRY_RUN=""
 
-pamac install --no-confirm $DRY_RUN \
-    libva \
+# UPGRADE="--no-upgrade"
+UPGRADE=""
+
+pamac install --no-confirm $UPGRADE $DRY_RUN \
+    hyprland \
+    hyprlang \
+    hyprpaper \
+     \
+    libva-nvidia-driver \
     pipewire \
     qt5-wayland \
     qt5ct \
     waybar \
     wireplumber \
     wofi \
+    xdg-desktop-portal-hyprland \
     xorg-xwayland
 pamac build --no-confirm $DRY_RUN \
     clipman \
-    hyprcursor-git \
     hypridle-git \
-    hyprland-git \
     hyprlock-git \
-    hyprpaper-git \
-    hyprshade \
-    libva-nvidia-driver-git \
-    xdg-desktop-portal-hyprland-git
+    hyprshade
