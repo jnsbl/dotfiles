5d9bb71c59e4ad3e51392543be4464e9c3b445e1
Jan Sibil <jnsbldev@gmail.com>
2024-05-13 17:30:27 +0200

    Update wilder-nvim.lua
    Use simpler (yet functional) configuration not requiring Python

--- neovim/lua/plugins/wilder-nvim.lua
+++ neovim/lua/plugins/wilder-nvim.lua
@@ -1,63 +1,21 @@
--- Fuzzy config (for Neovim or Vim with yarp)
+-- Basic config (for both Vim and Neovim)
 local wilder = require('wilder')
-wilder.setup({
-  modes = {':', '/', '?'},
-  accept_key = '<C-l>',
-  reject_key = '<C-h>',
-})
+wilder.setup({modes = {':', '/', '?'}})
 
 wilder.set_option('pipeline', {
   wilder.branch(
-    wilder.python_file_finder_pipeline({
-      file_command = {'fd', '-tf'},
-      dir_command = {'fd', '-td'},
-      -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-      -- found at https://github.com/nixprime/cpsm
-      filters = {'fuzzy_filter', 'difflib_sorter'},
-    }),
-    wilder.cmdline_pipeline({
-      fuzzy = 1,
-      set_pcre2_pattern = 1,
-    }),
-    wilder.python_search_pipeline({
-      pattern = 'fuzzy',
-    })
+    wilder.cmdline_pipeline(),
+    wilder.search_pipeline()
   ),
 })
 
-local highlighters = {
-  wilder.pcre2_highlighter(),
-  wilder.basic_highlighter(),
-}
+-- wilder.set_option('renderer', wilder.wildmenu_renderer({
+--   highlighter = wilder.basic_highlighter(),
+-- }))
 
-wilder.set_option('renderer', wilder.renderer_mux({
-  [':'] = wilder.popupmenu_renderer(
-    wilder.popupmenu_border_theme({
-      highlights = {
-        border = 'Normal',
-      },
-      border = 'rounded',
-      highlighter = highlighters,
-      left = {' ', wilder.popupmenu_devicons()},
-      right = {' ', wilder.popupmenu_scrollbar()},
-    })
-  ),
-  ['/'] = wilder.popupmenu_renderer(
-    wilder.popupmenu_border_theme({
-      highlights = {
-        border = 'Normal',
-      },
-      border = 'rounded',
-      highlighter = highlighters,
-    })
-  ),
-  ['?'] = wilder.popupmenu_renderer(
-    wilder.popupmenu_border_theme({
-      highlights = {
-        border = 'Normal',
-      },
-      border = 'rounded',
-      highlighter = highlighters,
-    })
-  ),
+-- Devicons for popupmenu
+wilder.set_option('renderer', wilder.popupmenu_renderer({
+  highlighter = wilder.basic_highlighter(),
+  left = {' ', wilder.popupmenu_devicons()},
+  right = {' ', wilder.popupmenu_scrollbar()},
 }))
