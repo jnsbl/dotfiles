9d9e262431eed2dc47c59af39ef9ee21ab9ca0fa
Jan Sibil <jnsbldev@gmail.com>
2025-05-10 17:08:09 +0200

    Customize fzf bindings
    Not setting history binding allows atuin be used when pressing ctrl-r
    
    (cherry picked from commit 6ecfc369ec7f2ecdfa5fcf575be903100e832dc6)

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -15,8 +15,7 @@ end
 set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub) (set --names | psub)'
 
 
-# Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
-fzf_configure_bindings
+fzf_configure_bindings --history= --directory=\ef
 
 # Doesn't erase autoloaded _fzf_* functions because they are not easily accessible once key bindings are erased
 function _fzf_uninstall --on-event fzf_uninstall
