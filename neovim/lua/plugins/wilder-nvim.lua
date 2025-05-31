60e76aeb832df313b7211c419e14712660d40ab0
Jan Sibil <jnsbldev@gmail.com>
2025-05-31 15:54:20 +0200

    Update wilder-nvim config

--- neovim/lua/plugins/wilder-nvim.lua
+++ neovim/lua/plugins/wilder-nvim.lua
@@ -7,20 +7,23 @@ return {
 
     wilder.set_option('pipeline', {
       wilder.branch(
-      wilder.cmdline_pipeline(),
-      wilder.search_pipeline()
+        wilder.cmdline_pipeline(),
+        wilder.search_pipeline()
       ),
     })
 
     -- wilder.set_option('renderer', wilder.wildmenu_renderer({
-      --   highlighter = wilder.basic_highlighter(),
-      -- }))
+    --   highlighter = wilder.basic_highlighter(),
+    -- }))
 
-      -- Devicons for popupmenu
-      wilder.set_option('renderer', wilder.popupmenu_renderer({
+    -- Devicons for popupmenu
+    wilder.set_option('renderer', wilder.popupmenu_renderer(
+      wilder.popupmenu_border_theme({
+        border = 'rounded',
         highlighter = wilder.basic_highlighter(),
         left = {' ', wilder.popupmenu_devicons()},
         right = {' ', wilder.popupmenu_scrollbar()},
-      }))
-    end,
+      })
+    ))
+  end,
   }
