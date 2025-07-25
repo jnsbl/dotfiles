4e623d3f62811691428e14eb7fb00642f9a27185
Jan Sibil <jnsbldev@gmail.com>
2025-07-25 17:11:43 +0200

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,19 @@
-local wezterm = require('wezterm')
-require('tabline')
-require('smart-splits')
+local wezterm = require("wezterm")
+require("tabline")
+require("smart-splits")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'Maple Mono NF'
+config.font = wezterm.font("Maple Mono NF")
 config.font_size = 11
 
-config.color_scheme = 'Monokai (base16)'
+config.color_scheme = "Monokai (base16)"
 
 config.window_padding = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '0.25cell',
-  bottom = '0.25cell',
+	left = "0.5cell",
+	right = "0.5cell",
+	top = "0.25cell",
+	bottom = "0.25cell",
 }
 
 config.hide_tab_bar_if_only_one_tab = true
